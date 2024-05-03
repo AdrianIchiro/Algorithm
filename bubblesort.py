f4e297ffa1cf77e96e03070f933973dbcb2b456e
AdrianIchiro <adrianfahren10@gmail.com>
2024-05-03 10:44:43 +0700

    bubblesort

--- bubblesort.py
+++ bubblesort.py
@@ -1,1 +1,21 @@
-print('hello world')
+def bubblesort(isArray):
+    n = len(isArray)
+
+    for i in range(n):
+        for j in range(n-1):
+            if isArray[j] > isArray[j+1]:
+                isArray[j], isArray[j+1] = isArray[j+1], isArray[j]
+        
+    
+
+
+
+angka = [10, 9, 2, 5, 6]
+print(angka)
+bubblesort(angka)
+print(angka)
+
+buah = ['banana', 'apple', 'purple']
+print(buah)
+bubblesort(buah)
+print(buah)
